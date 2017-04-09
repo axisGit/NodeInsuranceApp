f1a1213e631e52d10e82ae0cb89a1695d401ee0b
Jonathan Phipps <jphipps@wirelessprv-10-194-152-157.near.illinois.edu>
2017-04-09 16:48:42 -0500

    added account filtering

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-//var accountData = require('../data/axisUScomp.json');
+var accountData = require('../data/axisUScomp.json');
 var tornadoData = require('../data/tornComp.json');
 var windData = require('../data/windComp.json');
 var hailData = require('../data/hailComp.json');
+var earthquakeData = require('../data/earthquakeComp.json');
 
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -14,34 +15,34 @@ router.get('/location-test', function(req, res, next) {
    res.json(axisUS);
 });
 
-// router.post('/location', function(req, res, next) {
-//    var lat1 = req.body.lat1;
-//    var lon1 = req.body.lon1;
+router.post('/location', function(req, res, next) {
+   var lat1 = req.body.lat1;
+   var lon1 = req.body.lon1;
 
-//    var lat2 = req.body.lat2;
-//    var lon2 = req.body.lon2;
+   var lat2 = req.body.lat2;
+   var lon2 = req.body.lon2;
 
-//    var new_json;
+   console.log("lat1 = " + lat1);
 
-//    var i = 0;
-//    for (i = 0; i < axisUS.length; i++) {
-//        var cur_lat = (number)accountData["LATITUDE"][i];
-//        var cur_lon = (number)accountData["LONGITUDE"][i];
-//        if (cur_lat >= lat1 && cur_lat <= lat2 && cur_lon >= lon1 && cur_lon <= lon2) {
-//            var cur_json = {"ID":accountData["ID"][i], 
-//                            "PREMIUM":accountData["PREMIUM"][i], 
-//                            "TIV":accountData["TIV"][i], 
-//                            "DED":accountData["DED"][i], 
-//                            "CITY":accountData["CITY"][i], 
-//                            "POSTALCODE":accountData["POSTALCODE"][i]
-//                            "LATITUDE":accountData["LATITUDE"][i]
-//                            "LONGITUDE":accountData["LONGITUDE"][i]};
-//             new_json += cur_json;
-//        }
-//    }
+   var new_json = {"ID":[], "PREMIUM":[], "TIV":[], "DED":[], "CITY":[], "POSTALCODE":[], "LATITUDE":[], "LONGITUDE":[]};
 
-//    res.json(new_json);
-// });
+   var i = 0;
+   for (i = 0; i < accountData["LATITUDE"].length; i++) {
+       var cur_lat = accountData["LATITUDE"][i];
+       var cur_lon = accountData["LONGITUDE"][i];
+       if (cur_lat >= lat1 && cur_lat <= lat2 && cur_lon >= lon1 && cur_lon <= lon2) {
+            new_json["ID"].push(accountData["ID"][i]);
+            new_json["PREMIUM"].push(accountData["PREMIUM"][i]);
+            new_json["TIV"].push(accountData["TIV"][i]);
+            new_json["DED"].push(accountData["DED"][i]);
+            new_json["CITY"].push(accountData["CITY"][i]);
+            new_json["POSTALCODE"].push(accountData["POSTALCODE"][i]);
+            new_json["LATITUDE"].push(accountData["LATITUDE"][i]);
+            new_json["LONGITUDE"].push(accountData["LONGITUDE"][i]);
+       }
+   }
+   res.json(new_json);
+});
 
 router.get('/disasters', function(req, res, next) {
    var type = req.query.type;
@@ -51,6 +52,8 @@ router.get('/disasters', function(req, res, next) {
       res.json(tornadoData);
    else if(type == "hail")
       res.json(hailData);
+   else if(type == "earthquake")
+      res.json(earthquakeData);
    return next({
       success: false,
       message: "Not valid"
